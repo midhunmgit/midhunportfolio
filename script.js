dd9598706b3e04e164cabd23356d12f6c06e25df
midhunmgit <102047891+midhunmgit@users.noreply.github.com>
2022-05-27 19:23:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// typing animations
+var typed = new Typed(".typing", {
+    strings: [" Student", "Competitive Programmer", "Web Blogger"]
+	,typeSpeed: 150,
+    backSpeed: 60,
+    loop: true
+})
+
+var typed = new Typed(".typing-2", {
+    strings: [" CS Student at MGIT"]
+	,typeSpeed: 99,
+    backSpeed: 58,
+    loop: true
+})
